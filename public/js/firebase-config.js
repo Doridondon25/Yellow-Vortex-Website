b1e736bdfe73b4f0ebf2c857dc3a97eaa6cfb51a
Doridondon25 <dori.donitza@gmail.com>
2025-04-13 22:42:38 +0300

    Initial commit

---
+++ public/js/firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyC59vWDbdtLqfRTlqQWpdrrt2Eia36PqH8",
+    authDomain: "yellow-vortex.firebaseapp.com",
+    databaseURL: "https://yellow-vortex-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "yellow-vortex",
+    storageBucket: "yellow-vortex.firebasestorage.app",
+    messagingSenderId: "1007464214882",
+    appId: "1:1007464214882:web:c1e7b0e1a266309b94bc3b",
+    measurementId: "G-V76FRQK0FR"
+};
